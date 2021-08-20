b312db8aa3414e92957cf36ec7067572164eb281
Emanuele Palombo <muttley.bd@gmail.com>
2021-08-20 15:45:34 +0200

    AdcNode allow set callbacks
    setReadMeasurementFunc and setSendMeasurementFunc are now called if set

--- lib/Nodes/AdcNode.cpp
+++ lib/Nodes/AdcNode.cpp
@@ -8,11 +8,15 @@ extern int __get_adc_mode();
  *   note: check what happen when no wire is connected to ADC pin
  */
 
-AdcNode::AdcNode(const char *id, const char *name,
-                 uint32_t readInterval, float sendOnChangeAbs,
+AdcNode::AdcNode(const char *id,
+                 const char *name,
+                 uint32_t readInterval,
+                 float sendOnChangeAbs,
+                 const SensorBase<float>::ReadMeasurementFunc &readMeasurementFunc,
+                 const SensorBase<float>::SendMeasurementFunc &sendMeasurementFunc,
                  const SensorBase<float>::OnChangeFunc &onChangeFunc)
-        : BaseNode(id, name, "photoresistorNode"),
-          SensorBase(id, readInterval, 0, sendOnChangeAbs, nullptr, nullptr, onChangeFunc) {
+        : BaseNode(id, name, "adc"),
+          SensorBase(id, readInterval, 0, sendOnChangeAbs, readMeasurementFunc, sendMeasurementFunc, onChangeFunc) {
 
     // ADC pin used to measure VCC (e.g. on battery)
     // true if previously called macro ADC_MODE(ADC_VCC)
@@ -22,9 +26,9 @@ AdcNode::AdcNode(const char *id, const char *name,
 void AdcNode::setup() {
 
     advertise(getId())
-    .setDatatype("float")
-    .setFormat("0:1.00")
-    .setUnit(cUnitVolt);
+            .setDatatype("float")
+            .setFormat("0:1.00")
+            .setUnit(cUnitVolt);
 }
 
 void AdcNode::loop() {
@@ -32,12 +36,21 @@ void AdcNode::loop() {
 }
 
 float AdcNode::readMeasurement() {
+
+    if (mReadMeasurementFunc) {
+        return mReadMeasurementFunc();
+    }
+
     return static_cast<float>(mReadVcc ? ESP.getVcc() : analogRead(A0)) / 1024.0f;
 }
 
 void AdcNode::sendMeasurement(float value) const {
 
-    if(Homie.isConnected()) {
+    if (mSendMeasurementFunc) {
+        return mSendMeasurementFunc(value);
+    }
+
+    if (Homie.isConnected()) {
         setProperty(getId()).send(String(value));
     }
 }
--- lib/Nodes/AdcNode.hpp
+++ lib/Nodes/AdcNode.hpp
@@ -10,8 +10,10 @@ private:
 
 public:
     explicit AdcNode(const char *id, const char *name,
-            uint32_t readInterval = 1 * 1000UL, float sendOnChangeAbs = 0.06f,
-            const SensorBase<float>::OnChangeFunc &onChangeFunc = [](bool value) { return true; });
+                     uint32_t readInterval = 1 * 1000UL, float sendOnChangeAbs = 0.06f,
+                     const SensorBase<float>::ReadMeasurementFunc &readMeasurementFunc = nullptr,
+                     const SensorBase<float>::SendMeasurementFunc &sendMeasurementFunc = nullptr,
+                     const SensorBase<float>::OnChangeFunc &onChangeFunc = [](bool value) { return true; });
 
     void setup() override;
 
